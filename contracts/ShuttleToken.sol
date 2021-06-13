1d78799f2756b9be48441aa29878784c739419c2
samflynn19121@gmail.com <samflynn19121@gmail.com>
2021-06-13 21:54:17 +0200

    compiled with several warnings

--- contracts/ShuttleToken.sol
+++ contracts/ShuttleToken.sol
@@ -3,7 +3,7 @@ pragma solidity 0.6.12;
 import "./libs/BEP20.sol";
 
 // ShutleToken with Governance.
-contract ShutleToken is BEP20('Shutle', 'SHUTTLE') {
+contract ShuttleToken is BEP20('Shutle', 'SHUTTLE') {
     /// @notice Creates `_amount` token to `_to`. Must only be called by the owner (MasterChef).
     function mint(address _to, uint256 _amount) public onlyOwner {
         _mint(_to, _amount);
---
+++ test/ShuttleToken.test.js
@@ -0,0 +1,15 @@
+const { assert } = require("chai");
+
+const ShuttleToken = artifacts.require('ShuttleToken');
+
+contract('ShuttleToken', ([alice, bob, carol, dev, minter]) => {
+    beforeEach(async () => {
+        this.shuttle = await ShuttleToken.new({ from: minter });
+    });
+
+
+    it('mint', async () => {
+        await this.cake.mint(alice, 1000, { from: minter });
+        assert.equal((await this.shuttle.balanceOf(alice)).toString(), '1000');
+    })
+});
